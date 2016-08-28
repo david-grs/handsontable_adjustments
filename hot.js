444e23074d63e9b97663fe5d363cb36b73cb2393
David G <david.a.grs@gmail.com>
2016-08-28 23:21:43 +0200

    adding some data

--- hot.js
+++ hot.js
@@ -1,12 +1,16 @@
 $(document).ready(function ()
 {
+    var rows = [[1.0, "01/01/1988", 2, 3, 4],
+               [1.23, "01/01/1988", 0.33, 2, 4.4],
+               [11.11, "01/01/1988", 20.239, 3.3, 1]];
+    
     var onAdjustment = function(row, col)
     {
 
     };
 
     var hot = new Handsontable(document.getElementById("hot"), {
-        data: [[1.0, "01/01/1988", 2, 3, 4]],
+        data: rows,
         colHeaders: ["Price", "Date", "1D Chg", "YTD Chg", "Vol BTC"],
         rowHeaders: true,
         sortIndicator: true,
@@ -16,8 +20,8 @@ $(document).ready(function ()
         columns: [
         {type: 'numeric', format: '$0,0.00'},
         {type: 'date', dateFormat: 'DD/MM/YYYY', correctFormat: true},
-        {type: 'numeric', format: '0.00%'},
-        {type: 'numeric', format: '0.00%'},
+        {type: 'numeric', format: '0.00'},
+        {type: 'numeric', format: '0.00'},
         {type: 'numeric', format: '0.00'}
         ]
     });
